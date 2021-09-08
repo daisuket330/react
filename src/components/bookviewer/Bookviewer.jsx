8a2f3e8e0e1bd7e316cd597ed4c07744fbc32229
Treveon Edwards <fasuke322@gmail.com>
2021-09-08 15:52:34 -0400

    seperated book display into different components

--- src/components/bookviewer/Bookviewer.jsx
+++ src/components/bookviewer/Bookviewer.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-
+import Book from '../Book/Book';
 const BookViewer  = (props) => {
     return ( 
         <div className= "row row-spacer"> 
         <div className= "col-md-4">
-            {/*"button to move to previously viewd book"*/}
+            
             <button onClick={props.previousBook}>Previous</button>
         </div>
         <div className= "col-md-4">
-        {/*"display book with cover"*/}
-        <h1>{props.book.title}</h1>
-        <h4>{props.book.author}</h4>
+        
+        <Book book={props.book} />
         </div>
         <div className= "col-md-4">
-        {/*"button to move to nextbook"*/}
+        
         <button onClick={props.nextBook}>Next</button>
         </div>
     </div>
